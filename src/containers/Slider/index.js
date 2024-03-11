18492b8939b613ac28409933fab193651b32616d
abdoulkader <zarikader@gmail.com>
2024-03-11 09:35:43 +0100

    MA index slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -5,17 +5,19 @@ import { getMonth } from "../../helpers/Date";
 import "./style.scss";
 
 const Slider = () => {
-  const { data} = useData();
- 
- 
+  const { data } = useData();
+
   const [index, setIndex] = useState(0);
-  
+
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
 
   const nextCard = () => {
-    setTimeout(() => setIndex(index < (byDateDesc?.length ?? 0) - 1 ? index + 1 : 0), 5000);
+    setTimeout(
+      () => setIndex(index < (byDateDesc?.length ?? 0) - 1 ? index + 1 : 0),
+      5000
+    );
   };
   useEffect(() => {
     nextCard();
@@ -45,7 +47,7 @@ const Slider = () => {
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
-                  onChange={()=> setIndex(radioIdx)}
+                  onChange={() => setIndex(radioIdx)}
                 />
               ))}
             </div>
